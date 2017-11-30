c556f727c5cdd4ceb0edf0bad7507a0c7d0121a3
jaketothepast <jacob.windle@protonmail.com>
2017-11-30 12:39:18 -0500

    Open date file in append mode and write to it

--- notes.go
+++ notes.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 	"strconv"
+	"os"
 )
 
 // Append command line note to filename with date
@@ -20,4 +21,15 @@ func AppendToDateFile(configPath string, note string) {
 	notePath := path.Join(config.NoteDirectory, strings.Join(strs, "-"))
 	fmt.Println(notePath)
 
+	// Open file with append and wr flags set, 0600
+	f, err := os.OpenFile(notePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	if _, err := f.WriteString(note); err != nil {
+		panic(err)
+	}
 }
