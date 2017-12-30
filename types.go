fb46544acff027f821a27eec11dd2d13e803c8ae
jaketothepast <windlejacob12@gmail.com>
2017-12-30 16:02:16 -0500

    Adding an interface for notes to types, added date and children attributes to Note struct

--- types.go
+++ types.go
@@ -1,17 +1,39 @@
 package main
 
+require "time"
+
+type note interface {
+	// Notes should be able to save to a NotePage
+	Save(np NotePage) bool
+
+	// Notes can add children
+	AddChild(n Note) bool
+}
+
 // One leaf on the tree, represents a note/task
 type Note struct {
 	text string
 	priority int
-	task bool
+	date time.Date
+
+	// Notes can have child notes
+	children []Note
 }
 
 // Hold notes, can have child pages
 type NotePage struct {
 	title string
+
+	// A note page can have child note pages
 	children []NotePage
+
 	filename string
+
+	// A note page has many notes
 	notes []Note
+}
+
+func main() {
+
 }
 
